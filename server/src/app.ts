318cb43a509ba14b527cf6e5fbfc8e0b3d8f86d9
Rohit Shelke <rohitshelke11@gmail.com>
2022-05-06 11:39:20 +0530

    stop tracking .env, added mongoDB connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,24 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import {MongoClient} from "mongodb";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT;
+const mongoURL:string = String(process.env.MONGODB_URI);
+const client: MongoClient  = new MongoClient(mongoURL);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Pel Dunga");
 });
 
-app.listen(port, (): void => {
+app.listen(port, async(): Promise<void> => {
+  try {
+    await client.connect();
+    console.log('Successfully connected to MongoDB');
+} catch (e) {
+    console.error(e);
+}
   console.log(`🐵 Server Running at http://localhost:${port} 🐵`);
 });
