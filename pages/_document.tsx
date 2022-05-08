22ef50ba6ecafdd19126536c7645806c1e179427
Manthan Kuber <manthankuber873@gmail.com>
2022-05-08 09:31:00 +0530

    feat: add styled components + TS + SSR setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,12 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   render() {
@@ -16,5 +24,33 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
+  }
+
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
