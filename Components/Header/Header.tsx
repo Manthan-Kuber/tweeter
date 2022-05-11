3274449ebe1d1d37aa5407258f1e95c4bf753a7a
Manthan Kuber <manthankuber873@gmail.com>
2022-05-11 19:08:30 +0530

    feat: lifted the navbar state upto header

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -2,14 +2,25 @@ import Image from "next/image";
 import Navbar from "./Navbar/Navbar";
 import { StyledHeader } from "./Header.styles";
 import Profile from "./Profile";
+import useWindowSize from "../../Hooks/useWindowDimensions";
+import { FaCompass } from "react-icons/fa";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { AiFillHome } from "react-icons/ai";
+
+const NavList = [
+  { id: 1, name: "Home",url:"/", icon: <AiFillHome size={20} /> },
+  { id: 2, name: "Explore",url:"/explore", icon: <FaCompass size={20} /> },
+  { id: 3, name: "Bookmarks",url:"/bookmarks", icon: <BsFillBookmarkFill size={20} /> },
+];
 
 
 type Props = {};
 const Header = (props: Props) => {
+  const {width} = useWindowSize();
   return (
     <StyledHeader>
       <Image src="/tweeter.svg" width={"100%"} height={30} />
-      <Navbar />
+      { width! > 800  && <Navbar NavList={NavList} />}
       <Profile/>
     </StyledHeader>
   );
--- Components/Header/Navbar/Navbar.tsx
+++ Components/Header/Navbar/Navbar.tsx
@@ -1,22 +1,19 @@
 import { StyledUl, StyledNav, UnderlinedDiv, Li } from "./Navbar.styles";
-import { FaCompass } from "react-icons/fa";
-import { BsFillBookmarkFill } from "react-icons/bs";
-import { AiFillHome } from "react-icons/ai";
+
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import useWindowDimensions from "../../../Hooks/useWindowDimensions";
 
-const NavList = [
-  { id: 1, name: "Home",url:"/", icon: <AiFillHome size={20} /> },
-  { id: 2, name: "Explore",url:"/explore", icon: <FaCompass size={20} /> },
-  { id: 3, name: "Bookmarks",url:"/bookmarks", icon: <BsFillBookmarkFill size={20} /> },
-];
-
-type Props = {};
-const Navbar = (props: Props) => {
+interface Props  {
+  NavList: {
+    id: number;
+    name: string;
+    url: string;
+    icon: JSX.Element;
+  }[];
+};
+const Navbar = ({NavList}: Props) => {
   const router = useRouter();
-  const { width } = useWindowDimensions();
   const [activeTab, setActiveTab] = useState(NavList[0].name);
   return (
     <StyledNav>
@@ -30,7 +27,7 @@ const Navbar = (props: Props) => {
             }}
             active={activeTab === item.name ? true : false}
           >
-            {width! < 800 ? item.icon : item.name}
+            {item.name}
             {activeTab === item.name && (
               <UnderlinedDiv as={motion.div} layoutId="underlinedDiv" />
             )}
