d2db2c508366f967cbe8053ae53ef814d5d7fb08
Manthan Kuber <manthankuber873@gmail.com>
2022-05-11 06:00:39 +0530

    fix: removed get initial props from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,9 @@
 import Document, {
-  DocumentContext,
-  DocumentInitialProps,
   Head,
   Html,
   Main,
   NextScript,
 } from "next/document";
-import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   render() {
@@ -24,33 +21,5 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
-
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
   }
 }
