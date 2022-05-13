df497d9db3e822210a7209afe80e9fca9cfa80fc
Manthan Kuber <manthankuber873@gmail.com>
2022-05-13 06:10:20 +0530

    fix: fonts import  in document

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -32,10 +32,6 @@ function Layout({
       <Head>
         <title>Tweeter</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Noto+Sans:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <GlobalStyles />
       <Header
