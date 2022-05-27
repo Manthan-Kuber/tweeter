25c6a21f6c04c7351be9e43c6c2c2244ac4ef042
Manthan Kuber <manthankuber873@gmail.com>
2022-05-27 10:20:20 +0530

    feat(misc): unique email for userSchema

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,23 +1,26 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from "mongoose";
 
 interface IUser {
-    firstname: string
-    lastname: string
-    email: string
-    username: string
-    mobile?: string
-    dob?: string
-    bio?: string
+  firstname: string;
+  lastname: string;
+  email: string;
+  username: string;
+  mobile?: string;
+  dob?: string;
+  bio?: string;
 }
 
-const userSchema = new Schema<IUser>({
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    email: {type: String, required: true},
-    username: {type: String, required: true},
+const userSchema = new Schema<IUser>(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    username: { type: String, required: true },
     mobile: String,
     dob: String,
-    bio: String 
-}, {timestamps: true});
+    bio: String,
+  },
+  { timestamps: true }
+);
 
-export default model<IUser>('User', userSchema);
+export default model<IUser>("User", userSchema);
