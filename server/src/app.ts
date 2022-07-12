940b866141c24c2a5c80e86d434a9ebb94094995
Rohit Shelke <rohitshelke11@gmail.com>
2022-07-12 12:16:38 +0530

    feat: added profile APIs

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,7 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import tweetRoutes from "./routes/tweetRoutes";
 import commentRoutes from "./routes/commentRoutes";
-// import profileRoutes from "./routes/profileRoutes";
+import profileRoutes from "./routes/profileRoutes";
 import homeRoutes from "./routes/homeRoutes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -37,7 +37,7 @@ app.use(authRoutes);
 app.use("/users", userRoutes);
 app.use("/tweets", tweetRoutes);
 app.use("/comment", commentRoutes);
-// app.use("/profile", profileRoutes);
+app.use("/profile", profileRoutes);
 app.use("/home", homeRoutes);
 // app.set("view engine",viewEngine_name)
 
