b9bb522e4fa1b12372812045e49c0b17d9af06bd
Manthan Kuber <manthankuber873@gmail.com>
2022-10-14 19:33:52 +0530

    feat: add github profile links in footer

--- client/Components/Footer/Footer.tsx
+++ client/Components/Footer/Footer.tsx
@@ -1,16 +1,29 @@
 import styled from "styled-components";
 
-const Footer = ({footerBg}: { footerBg?: string }) => {
+const Footer = ({ footerBg }: { footerBg?: string }) => {
   return (
     <StyledFooter footerBg={footerBg}>
       <FooterText>
         Created by{" "}
         <b>
-          <u>Manthan Kuber</u>
+          <a
+            href="https://github.com/Manthan-Kuber"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>Manthan Kuber</u>
+          </a>
         </b>
         <b>
           {" "}
-          & <u>Rohit Shelke</u>
+          &{" "}
+          <a
+            href="https://github.com/RohitShelkeBot"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>Rohit Shelke</u>
+          </a>
         </b>
       </FooterText>
     </StyledFooter>
@@ -19,10 +32,10 @@ const Footer = ({footerBg}: { footerBg?: string }) => {
 
 export default Footer;
 
-export const StyledFooter = styled.footer<{footerBg?:string}>`
+export const StyledFooter = styled.footer<{ footerBg?: string }>`
   margin-top: auto;
   padding-block: 1.5rem;
-  background-color: ${props => props.footerBg || "#f2f2f2" };
+  background-color: ${(props) => props.footerBg || "#f2f2f2"};
 
   /* @media (min-width: 55em) {
     padding-bottom: 2.4rem;
